cbff0e6c0c556680b90abce4554e2eb9a9b1af41
daphne6 <bugehusi@ahk.jp>
2020-08-19 14:55:20 +0900

    first step

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 let birthYear = 2001
-if (birthYear % 400 ===0){
+if(birthYear % 400 ===0){
     document.write('うるう年です');
 }
 else{ 
- if (birthYear % 100 !== 0){
-    if(birthYear % 4 ===0){
-    document.write('うるう年です');
-    }
-    else {
-    document.write('うるう年ではありません');
-    }
- }
- else {
+ if(birthYear % 100 !== 0 && birthYear % 4 ===0){
+    document.write('うるう年です');}
+ else{
     document.write('うるう年ではありません');
  }
 }
